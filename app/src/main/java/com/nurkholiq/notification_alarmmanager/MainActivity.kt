666e81f7d181df448721496a5a0f6313bf6f68ef
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-09-21 12:40:27 +0700

    Modified MainActivity.kt - menampilkan showTimePicker pada btnSelectTime

--- app/src/main/java/com/nurkholiq/notification_alarmmanager/MainActivity.kt
+++ app/src/main/java/com/nurkholiq/notification_alarmmanager/MainActivity.kt
@@ -5,22 +5,59 @@ import android.app.NotificationManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.nurkholiq.notification_alarmmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var picker: MaterialTimePicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        supportActionBar?.title = "Notification Alarm"
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         craateNotificationChannel()
+
+        binding.btnSelectTime.setOnClickListener {
+
+            showTimePicker()
+
+        }
+
+        binding.btnSetAlarm.setOnClickListener {
+
+
+        }
+
+
+        binding.btnCancelAlarm.setOnClickListener {
+
+
+        }
+
+
+    }
+
+    private fun showTimePicker() {
+        picker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Select Alarm Time")
+            .build()
+
+        picker.show(supportFragmentManager, "foxandroid")
+
     }
 
     private fun craateNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name : CharSequence = "foxandroidReminderChannel"
+            val name: CharSequence = "foxandroidReminderChannel"
             val description = "Channel For Alarm Manager"
             val importance = NotificationManager.IMPORTANCE_HIGH
             val channel = NotificationChannel("foxandroid", name, importance)
@@ -28,6 +65,9 @@ class MainActivity : AppCompatActivity() {
             val notificationManager = getSystemService(
                 NotificationManager::class.java
             )
+
+            notificationManager.createNotificationChannel(channel)
+
         }
     }
 }
