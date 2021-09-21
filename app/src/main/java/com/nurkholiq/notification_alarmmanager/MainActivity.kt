f4854bc3df726f09d3f9c1b5ddde198437b672e4
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-09-21 10:29:07 +0700

    Create a new project with Notification using AlarmManager

---
+++ app/src/main/java/com/nurkholiq/notification_alarmmanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nurkholiq.notification_alarmmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
