441456f135bd4a24c4f9adb1c74bab8cb6cf7fb6
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-09-21 12:23:32 +0700

    Modified MainActivity.kt

--- app/src/main/java/com/nurkholiq/notification_alarmmanager/MainActivity.kt
+++ app/src/main/java/com/nurkholiq/notification_alarmmanager/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.nurkholiq.notification_alarmmanager
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nurkholiq.notification_alarmmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        craateNotificationChannel()
+    }
+
+    private fun craateNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name : CharSequence = "foxandroidReminderChannel"
+            val description = "Channel For Alarm Manager"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("foxandroid", name, importance)
+            channel.description = description
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            )
+        }
     }
 }
